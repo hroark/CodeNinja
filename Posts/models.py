ab5623b92bf8a18c998513a11ead3c34b214fe4d
rigamonk <607316+hroark@users.noreply.github.com>
2023-08-17 14:57:15 -0500

    more stuff

--- Posts/models.py
+++ Posts/models.py
@@ -1,3 +1,29 @@
+import datetime
+from enum import Enum
 from django.db import models
 
-# Create your models here.
+class postType(Enum):
+    request = 1
+    code = 2
+
+
+
+class basePost(models.Model):
+    title = models.CharField(max_length=50)
+    text = models.CharField(max_length=250)
+    createDate = models.DateTimeField("Date created")
+    language = models.CharField(max_length=10)
+    ##author = models.ForeignKey('User', on_delete=models.CASCADE)
+    pType = postType
+    
+    def __str__(self) -> str:
+        return self.Title
+    
+class requestPost(basePost):
+    pType = postType.request
+        
+class codePost(basePost):
+    ninjaPoints = models.IntegerField(default=0)
+    pType = postType.code
+    
+    
--- Posts/urls.py
+++ Posts/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from . import views
+
 urlpatterns = [
-    path("posts/", include("Posts.urls")),
-    path("admin/", admin.site.urls),
+    path("", views.index, name="index"),
 ]
