9b3cf2c890f63b42fbeb13a27562db8ee50943ab
pporter <pporter@rosensdiversified.com>
2023-08-18 13:02:12 -0500

    Index view

--- Posts/views.py
+++ Posts/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import basePost, requestPost,codePost
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    requestList = requestPost.objects.order_by("createDate")[:5]
+    codeList = codePost.objects.order_by("createDate")[:5]
+    template = loader.get_template('posts/index.html/')
+    context = {"Lastest Requests": requestList, "Latest Code": codeList}
+    return HttpResponse(template.render(context,request))
+    
+     
+
+##index page
+    #USer Stuff
+    #Nav BAr
+    #List of last 5 request and code posts
