e1cbb971c950b716b1334a4830c76bfe06ffe063
rigamonk <607316+hroark@users.noreply.github.com>
2023-08-27 17:04:01 -0500

    Posts display setup. Some styling.

--- CodeNinja/urls.py
+++ CodeNinja/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
-from django.conf.urls.static import static
-from django.urls.resolvers import settings 
+from Posts.views import index,detail
 
 urlpatterns = [
-    path('', include('Posts.urls')),
+    #path('', include('Posts.urls')),
+    path("", index),
     path("posts", include("Posts.urls")),
     path("admin", admin.site.urls),
+    path('<slug:slug>/',detail, name='detail'),
 ]
--- Posts/models.py
+++ Posts/models.py
@@ -1,5 +1,5 @@
-import datetime
 from enum import Enum
+from pyexpat import model
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -8,17 +8,25 @@ class postType(Enum):
     code = 2
 
 
+class Language(models.Model):
+    lang_name = models.CharField(max_length=10)
+    def __str__(self):
+        return f"{self.lang_name}"
 
 class basePost(models.Model):
     title = models.CharField(max_length=50)
     text = models.CharField(max_length=250)
-    createDate = models.DateTimeField("Date created")
-    language = models.CharField(max_length=10)
+    slug = models.SlugField(null=True)
+    createDate = models.DateTimeField(auto_now_add=True)
+    language = models.ForeignKey(Language, on_delete=models.CASCADE)
     author = models.ManyToManyField(User)
     pType = postType
     
+    class Meta:
+        ordering = ['-createDate']
+    
     def __str__(self) -> str:
-        return self.title
+        return f"{self.title} {self.author}"
 
 class requestPost(basePost):
     pType = postType.request
@@ -26,12 +34,6 @@ class requestPost(basePost):
     def __init__(self,*args,**kwargs):
         super(basePost, self).__init__(*args,**kwargs)
     
-    def __str__(self) -> str:
-        return super().__str__()
-
-
-
-
 
 class codePost(basePost):
     ninjaPoints = models.IntegerField(default=0)
